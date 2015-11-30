7adfb6a115a91129341b6b481dd294a9f325964c
fage <fage@juanpi.com>
2015-11-30 18:30:56 +0800

    添加注释

--- EasyImageAsset.php
+++ EasyImageAsset.php
@@ -3,6 +3,10 @@ namespace cliff363825\image;
 
 use yii\web\AssetBundle;
 
+/**
+ * Class EasyImageAsset
+ * @package cliff363825\image
+ */
 class EasyImageAsset extends AssetBundle
 {
     public $sourcePath = '@cliff363825/image/assets';
